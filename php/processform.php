f10761302a5d3d7b73215c1b08b375e74f232d4e
Inês Lopes <79641986+Nockinhas@users.noreply.github.com>
2021-11-28 20:27:42 +0000

    process form PHP

--- php/processform.php
+++ php/processform.php
@@ -1,23 +1,28 @@
 <?php
+
 $servername= "dbname=postgres user=postgres password=postgres host=localhost port=5432";
 $connection = pg_connect($servername);
 if (!$connection){
     die("Erro na ligacao");
 }
 
+$username = $_POST['Username'];
+$password = $_POST['Password'];
 
-$resultados= pg_query($connection, "select * from administrador") or die;
+echo $username;
+echo $password;
+
+/* $resultados= pg_query($connection, "select * from administrador") or die;
 $resultados= pg_fetch_all($resultados);
 foreach($resultados as $linha){
     print $linha['username'] . " " . $linha['password'];
     print "\n";
-}
+} */
 
-$resultados = pg_query($connection, "delete from administrador where username='Jose'");
-print pg_affected_rows($resultados); 
+/* $resultados = pg_query($connection, "delete from administrador where username='Jose'");
+print pg_affected_rows($resultados);  */
 
-/* $resultados = pg_query($connection, "insert into administrador values ('José' , 'zezoca123')");
-print pg_affected_rows($resultados); */
+$resultados = pg_query($connection, "insert into administrador values ('$username' , '$password')");
 
 //echo "Ligacao estabelecida!";    // do something herepg_close($connection);
 pg_close($connection);
